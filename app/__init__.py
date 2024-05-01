6add0a7956f3c970f855a9d6aa761de85efa118b
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2024-05-01 13:49:49 -0500

    ✨ [UPDATE] static config

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
+import os
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.router import router
 
+CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
+STATIC_PATH = os.path.join(CURRENT_PATH, "static")
 
 app: FastAPI = None
 
@@ -9,5 +13,6 @@ def get_app() -> FastAPI:
     global app
     if app is None:
         app = FastAPI(tags=["API"])
+        app.mount("/static", StaticFiles(directory=STATIC_PATH), name="static")
         app.include_router(router)
     return app
